2238cb5d1e5d757c54baf27acb9bd1ebb9067ab0
s1lv3r-f0x <ya.tini508@yandex.ru>
2023-04-18 12:37:50 +0400

    initialize project

---
+++ PomadoroTimer/Model/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Timers;
+
+namespace PomadoroTimer.Model
+{
+  public class Tracker
+  {
+    public string Note { get; set; }
+
+    private System.Timers.Timer timer;
+
+    public Tracker() 
+    {
+      this.timer = new System.Timers.Timer();
+    }
+
+    public void StartTimer() 
+    {
+      this.timer.Start();
+    }
+
+    public string StopTimer() 
+    {
+      this.timer.Stop();
+      return timer.Interval.ToString();
+    }
+  }
+}
