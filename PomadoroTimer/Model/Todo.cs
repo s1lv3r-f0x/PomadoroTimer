6b7c93db1fdde7b6b0bb3acdd7954a904e46b8cc
s1lv3r-f0x <ya.tini508@yandex.ru>
2023-05-09 21:25:56 +0400

    Полная переработка проекта:-- добавлен класс TodoStopwatch;
    -- добавлен класс Todo;Todo;-- удален класс Timer;
    -- удалены лишние элементы.

---
+++ PomadoroTimer/Model/Todo.cs
@@ -0,0 +1,40 @@
+﻿namespace PomadoroTimer.Model
+{
+  public class Todo
+  {
+    private TodoStopwatch stopwatch;
+
+    public string Title;
+
+    public string Duration;
+
+    public Todo(string title) 
+    {
+      this.stopwatch = new TodoStopwatch();
+      this.Title = title;
+    }
+
+    public void StartWatch()
+    {
+      stopwatch.Start();
+    }
+
+    public void PauseWatch()
+    {
+      stopwatch.Pause();
+      this.UpdateDuration();
+    }
+
+    public void StopWatch()
+    {
+      stopwatch.Stop();
+      this.UpdateDuration();
+    }
+
+    public void UpdateDuration()
+    {
+      var duration = stopwatch.GetDuration();
+      this.Duration = String.Format("{0:00}:{1:00}", duration.Hours, duration.Minutes);
+    }
+  }
+}
---
+++ PomadoroTimer/Model/TodoStopwatch.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace PomadoroTimer.Model
+{
+  public class TodoStopwatch 
+  {
+    private readonly Stopwatch stopwatch;
+
+    private TimeSpan duration;
+
+    private DateTime endDate;
+
+    public readonly DateTime StartDate;
+
+    public TodoStopwatch() 
+    {
+      this.StartDate = DateTime.Now.ToLocalTime();
+      this.stopwatch = new Stopwatch();
+    }
+
+    public void Start()
+    {
+      this.stopwatch.Start();
+    }
+
+    public void Pause()
+    {
+      this.stopwatch.Stop();
+      this.duration += stopwatch.Elapsed;
+    }
+
+    public void Stop()
+    {
+      this.stopwatch.Stop();
+      this.endDate = DateTime.Now.ToLocalTime();
+    }
+
+    public TimeSpan GetDuration()
+    {
+      return this.duration;
+    }
+  }
+}
